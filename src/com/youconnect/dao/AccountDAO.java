76bfbf46b31be29cc5e2139e06d2a25fa3635598
prasanna <prajend1@uncc.edu>
2016-03-26 16:11:02 -0400

    Commit2

---
+++ src/com/youconnect/dao/AccountDAO.java
@@ -0,0 +1,40 @@
+package com.youconnect.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.youconnect.bean.Account;
+import com.youconnect.bean.Member;
+
+public class AccountDAO {
+	
+
+	
+	private SqlSessionFactory sqlSessionFactory; 
+	
+	public AccountDAO(){
+		sqlSessionFactory = MyBatisConnectionFactory.getSqlSessionFactory();
+	}
+	
+
+	/**
+	 * Returns the list of all Contact instances from the database.
+	 * @return the list of all Contact instances from the database.
+	 */
+	@SuppressWarnings("unchecked")
+	public Account selectLogin(Account acct){
+
+		SqlSession session = sqlSessionFactory.openSession();
+		
+		try {
+			acct = (Account)session.selectOne("Accounts.getAll",acct);
+			return acct;
+		} finally {
+			session.close();
+		}
+	}
+
+	
+}
